a184d76808b2741ce1d04fa698cd26fb23edf2e4
Semyon Rozhkov <semro@outlook.com>
2023-07-20 22:58:36 +0300

    feat: 1, 2 finished, 3 to finish

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -13,20 +13,61 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+import {
+  equals,
+  allPass,
+  prop,
+  compose,
+  values,
+  length,
+  lte,
+  filter,
+  count,
+  all,
+} from "ramda";
+import { SHAPES, COLORS } from "../constants";
 
-    return star === 'red' && square === 'green';
-};
+// Геттеры
+const getStar = prop(SHAPES.STAR);
+const getSquare = prop(SHAPES.SQUARE);
+const getTriangle = prop(SHAPES.TRIANGLE);
+const getCircle = prop(SHAPES.CIRCLE);
+
+// Предикаты
+const isWhite = equals(COLORS.WHITE);
+const isRed = equals(COLORS.RED);
+const isOrange = equals(COLORS.ORANGE);
+const isGreen = equals(COLORS.GREEN);
+const isBlue = equals(COLORS.BLUE);
+
+// Предикаты посложней
+const isRedStar = compose(isRed, getStar);
+const isGreenSquare = compose(isGreen, getSquare);
+const isWhiteTriangle = compose(isWhite, getTriangle);
+const isWhiteCircle = compose(isWhite, getCircle);
+
+const isMoreOrEquals2 = lte(2);
+
+export const validateFieldN1 = allPass([
+  isRedStar,
+  isGreenSquare,
+  isWhiteTriangle,
+  isWhiteCircle,
+]);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = compose(isMoreOrEquals2, count(isGreen), values);
+
+const log = (...args) => {
+  console.log(args);
+  return args;
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = all(equals(2))([
+  compose(count(isRed), values),
+  compose(count(isBlue), values),
+]);
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
 export const validateFieldN4 = () => false;
