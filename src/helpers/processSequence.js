ac32306cba0ba5249b7321ec6639aa87bb06007d
Semyon Rozhkov <semro@outlook.com>
2023-07-23 16:57:34 +0300

    feat: `processSequence` done

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -15,47 +15,86 @@
  * Ответ будет приходить в поле {result}
  */
 import Api from "../tools/api";
+import {
+  __,
+  allPass,
+  prop,
+  compose,
+  length,
+  not,
+  trim,
+  split,
+  join,
+  test,
+  ifElse,
+  gt,
+  lt,
+  tap,
+  andThen,
+  otherwise,
+  assoc,
+  mathMod,
+  partial,
+} from "ramda";
 
 const api = new Api();
+const API_BASE_URL = "https://api.tech/numbers/base";
+const API_ANIMAL_URL = "https://animals.tech";
 
-/**
- * Я – пример, удали меня
- */
-const wait = (time) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
+const removeWhitespace = compose(join(""), split(/\s+/), trim);
+
+const getLength = compose(length);
+const getRoundNumber = compose(Math.round, Number);
+const getApiResult = compose(String, prop("result"));
+const getSquareNumber = (n) => n ** 2;
+const getMod3 = (n) => mathMod(n, 3);
+
+const isGreaterThan2 = gt(__, 2);
+const isLessThan10 = lt(__, 10);
+const isValidRange = compose(
+  allPass([isGreaterThan2, isLessThan10]),
+  getLength
+);
+const isFloatingNumber = test(/[+-]?([0-9]*[.])?[0-9]+/);
+const isPositive = compose(not, test(/^-/));
+const isValid = allPass([isValidRange, isFloatingNumber, isPositive]);
+
+const createApiBaseQuery = assoc("number", __, { from: 10, to: 2 });
+const createApiAnimalUrl = (id) => new URL(id, API_ANIMAL_URL);
+
+const apiGetBinaryNumber = compose(api.get(API_BASE_URL), createApiBaseQuery);
+const apiGetAnimal = compose(api.get(__, {}), createApiAnimalUrl);
+
+const thenGetApiResult = andThen(getApiResult);
 
 const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
-  /**
-   * Я – пример, удали меня
-   */
-  writeLog(value);
+  const tapLog = tap(writeLog);
+  const thenTapLog = andThen(tapLog);
+  const handleValidationError = partial(handleError, ["ValidationError"]);
 
-  api
-    .get("https://api.tech/numbers/base", {
-      from: 2,
-      to: 10,
-      number: "01011010101",
-    })
-    .then(({ result }) => {
-      writeLog(result);
-    });
+  const continueProcessing = compose(
+    otherwise(handleError),
+    andThen(handleSuccess),
+    thenGetApiResult,
+    andThen(apiGetAnimal),
+    thenTapLog,
+    andThen(getMod3),
+    thenTapLog,
+    andThen(getSquareNumber),
+    thenTapLog,
+    andThen(getLength),
+    thenTapLog,
+    thenGetApiResult,
+    apiGetBinaryNumber,
+    tapLog,
+    getRoundNumber
+  );
 
-  wait(2500)
-    .then(() => {
-      writeLog("SecondLog");
-
-      return wait(1500);
-    })
-    .then(() => {
-      writeLog("ThirdLog");
-
-      return wait(400);
-    })
-    .then(() => {
-      handleSuccess("Done");
-    });
+  compose(
+    ifElse(isValid, continueProcessing, handleValidationError),
+    removeWhitespace,
+    tapLog
+  )(value);
 };
 
 export default processSequence;
